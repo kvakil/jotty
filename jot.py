58e35c85bab94fd2494954e01a8f99ecb12df124
kvakil <kvakil@berkeley.edu>
2016-09-28 14:12:47 -0700

    added doctests

--- jot.py
+++ jot.py
@@ -6,6 +6,24 @@ ski_I = lambda x: x
 ski_K = lambda x: lambda y: x
 
 def jot_to_py(code):
+    """Tests:
+    >>> jot_to_py('')('Identity!')
+    'Identity!'
+    >>> # K combinator
+    >>> jot_to_py('11100')(42)(17)
+    42
+    >>> # S combinator
+    >>> add = lambda x: lambda y: x + y
+    >>> double = lambda x: add(x)(x)
+    >>> jot_to_py('11111000')(add)(double)(4)
+    12
+    >>> # infinite loop
+    >>> jot_to_py('11111111000111111111000001111111110000011111110001111111110\
+000011111111100000')
+    Traceback (most recent call last):
+        ...
+    RecursionError: maximum recursion depth exceeded
+    """
     def process_char(accum, char):
         if char == '1':
             return lambda f: lambda a: accum(f(a))
@@ -13,3 +31,7 @@ def jot_to_py(code):
             return accum(ski_S)(ski_K)
 
     return reduce(process_char, code, ski_I)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
